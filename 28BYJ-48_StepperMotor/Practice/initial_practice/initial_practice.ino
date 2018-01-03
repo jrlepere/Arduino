819cf7d9867069354ab330db71f3b240ce29b9c3
jrlepere <jrlepere1@gmail.com>
2018-01-03 11:43:33 -0800

    28BYJ-48 Practice

--- 28BYJ-48_StepperMotor/Practice/initial_practice/initial_practice.ino
+++ 28BYJ-48_StepperMotor/Practice/initial_practice/initial_practice.ino
@@ -6,12 +6,11 @@
 #define IN4 11
 
 const int stepPerRev = 2048;
-const int maxStepSpeed = 16;
+const int stepSpeed = 16;
 
 Stepper s = Stepper(stepPerRev,IN1,IN2,IN3,IN4);
 
 void setup() {
-  int stepSpeed = maxStepSpeed - 0;
   s.setSpeed(stepSpeed);
   Serial.begin(9600);
 }
